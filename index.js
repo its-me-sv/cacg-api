0625044e625312332ad164c1358750c4c86bc1af
Suraj Vijay <surajcpp115@gmail.com>
2022-04-17 21:38:39 +0530

    defined routes for server

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const morgan = require("morgan");
+const rateLimitter = require("express-rate-limit");
+
+const morganConfig = require('./morgan.config');
+const serverRateLimiter = rateLimitter({
+  windowMs: 5 * 1000,
+  max: 3,
+  legacyHeaders: false
+});
+
+const app = express();
+
+app.use(serverRateLimiter);
+app.use(morgan(morganConfig));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.status(200).json("Base route for CACG API");
+});
+
+app.post("/solve", (req, res) => {
+  return res.status(200).json("Solution route");
+});
+
+app.get("/*", (req, res) => {
+  res.redirect('/');
+});
+
+app.listen(5000, () => {
+  console.clear();
+  console.log("[SERVER] Listening to PORT 5000");
+});
