2fd28a99e6396ac4089d36d52fcddf09027899c1
Suraj Vijay <surajcpp115@gmail.com>
2022-04-24 12:19:30 +0530

    importing and using common functions in all languages

--- code-gens/ts.code-gen.js
+++ code-gens/ts.code-gen.js
@@ -1,5 +1,14 @@
+// custom
+const {getDistinct, findLowerBounds} = require('./common.code-gen');
+
 const tsCodeGen = (addend, augend, sum) => {
   const startTime = Date.now();
+  // finding distinct letters
+  const distinct_letters = getDistinct(addend + augend + sum);
+  // more than 10 distinct letters
+  if (distinct_letters.length > 10) return false;
+  // calculating the lower bounds
+  const lowerBounds = findLowerBounds(distinct_letters, [addend[0], augend[0], sum[0]]);
   // output string
   let outputString = '';
   const endTime = Date.now();
